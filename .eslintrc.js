21f174ebf735a8b6af1b6d1609ee18d1ab8af9fb
Romualdo Santos <romajslit@gmail.com>
2020-05-16 17:31:27 -0300

    Setup eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: ['plugin:react/recommended', 'standard', 'standard-react', 'plugin:prettier/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'never',
+        exports: 'never',
+        functions: 'ignore',
+      },
+    ],
+    'no-console': 'warn',
+    'no-unused-vars': 'warn',
+    'react/prop-types': 'warn',
+    semi: ['error', 'always'],
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+      },
+    ],
+    'prettier/prettier': 'error',
+  },
+};
